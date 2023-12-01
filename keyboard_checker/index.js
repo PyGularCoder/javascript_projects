b6881d28cfc38323a0d3cd0b547615c2e68f3b11
Muhammad Hafeezullah <146739943+PyGularCoder@users.noreply.github.com>
2023-12-01 21:45:27 +0000

    keyboard updated

--- keyboard_checker/index.js
+++ keyboard_checker/index.js
@@ -1,81 +1,46 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const keyboard = document.getElementById("keyboard");
-    let pressedKeyElement = null;
-
-    const layout = [
-        "Escape", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12",
-        "`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=",
-        "Backspace", "Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]",
-        "\\", "CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'",
-        "Enter", "ShiftLeft", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/",
-        "ControlLeft", "MetaLeft", "AltLeft", "Space", "AltRight", "ControlRight",
-        "ArrowLeft", "ArrowUp", "ArrowRight", "ArrowDown",
-    ];
-
-    layout.forEach((key) => {
-        const keyElement = document.createElement("div");
-        keyElement.classList.add("key");
-        if (key.length > 1) {
-            keyElement.classList.add("special");
-        }
-        if (key === " ") {
-            keyElement.classList.add("space");
-        }
-        if (["Enter", "ShiftLeft", "ShiftRight", "ControlLeft", "ControlRight"].includes(key)) {
-            keyElement.classList.add("large");
-        }
-        if (["ArrowLeft", "ArrowUp", "ArrowRight", "ArrowDown"].includes(key)) {
-            keyElement.classList.add("bottom-row");
-        }
-        keyElement.textContent = key;
-        keyElement.addEventListener("click", () => onKeyPress(keyElement, key));
-        keyboard.appendChild(keyElement);
-    });
-
-    document.addEventListener("keydown", function (event) {
-        const keyPressed = event.key.toUpperCase();
-        const keyElement = getKeyElement(keyPressed);
-
-        if (!keyElement) {
-            const keyElementByCode = getKeyElement(event.code);
-            if (keyElementByCode) {
-                keyElementByCode.textContent = keyPressed;
-                keyElementByCode.classList.add("highlight");
-                pressedKeyElement = keyElementByCode;
-            }
-        } else {
-            highlightKey(keyElement);
-        }
-    });
-
-    document.addEventListener("keyup", function () {
-        if (pressedKeyElement) {
-            unhighlightKey(pressedKeyElement);
-            pressedKeyElement = null;
-        }
-    });
-
-    function getKeyElement(keyValue) {
-        return Array.from(keyboard.children).find(
-            (keyElement) => keyElement.textContent === keyValue
-        );
+document.addEventListener('keydown', function (event) {
+    const pressedKey = event.key;
+    const keyElement = document.querySelector(`.key.${getKeyClass(pressedKey)}`);
+  
+    if (keyElement) {
+      keyElement.classList.add('pressed');
     }
-
-    function onKeyPress(keyElement, keyValue) {
-        if (pressedKeyElement) {
-            unhighlightKey(pressedKeyElement);
-        }
-
-        highlightKey(keyElement);
-        alert(`You pressed the key: ${keyValue}`);
+  });
+  
+  document.addEventListener('keyup', function (event) {
+    const pressedKey = event.key;
+    const keyElement = document.querySelector(`.key.${getKeyClass(pressedKey)}`);
+  
+    if (keyElement) {
+      keyElement.classList.remove('pressed');
     }
-
-    function highlightKey(keyElement) {
-        keyElement.classList.add("highlight");
-        pressedKeyElement = keyElement;
+  });
+  
+  function getKeyClass(key) {
+    switch (key) {
+      case ' ':
+        return 'spacebar';
+      case 'Enter':
+        return 'enter';
+      case 'Backspace':
+        return 'backspace';
+      case 'Tab':
+        return 'tab';
+      case 'CapsLock':
+        return 'caps-lock';
+      case 'Shift':
+        return 'shift';
+        case 'Control':
+            return 'left-control';
+        case 'Meta':
+            return 'left-meta';
+        case 'Alt-left':
+            return 'left-alt';
+        case 'Alt-right':
+            return 'left-alt';
+      default:
+        return key.toLowerCase(); // Convert other keys to lowercase
     }
-
-    function unhighlightKey(keyElement) {
-        keyElement.classList.remove("highlight");
-    }
-});
+  }
+  
+  
